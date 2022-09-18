aa9caeaf6a87dd6cf89c2217f1e640b0663ce770
pradyumndaga <pradyumndaga28@gmail.com>
2022-09-18 22:03:48 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "../login/login.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 
-const routes: Routes = [
-  {path:"login", component: LoginComponent}
-]
+const routes: Routes = [{ path: 'login', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  declarations: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
